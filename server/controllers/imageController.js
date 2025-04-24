ea5b85b138c4c742657c5e257726a4d8e56e9731
Ram Charan <ramcharanchary6300@gmail.com>
2025-04-24 11:02:22 +0530

    Implemented user authentication

---
+++ server/controllers/imageController.js
@@ -0,0 +1,18 @@
+import userModel from "../models/userModel";
+
+export const generateImage = async (req, res) => {
+    try {
+        const { userId, prompt } = req.body;
+        const user = await userModel.findById(userId)
+        if (!user || !prompt) {
+            return res.json({ sucess: false, message: 'Missing Details' })
+        }
+        if(user.creditBalance===0||userModel.creditBalance<0){
+            return res.json({sucess:false,message:'No Credit Balance ',creditBalance:user.creditBalance})
+        }
+        
+    } catch (error) {
+        console.log(error.message)
+        res.json({ sucess: false, message: error.message })
+    }
+}
--- server/server.js
+++ server/server.js
@@ -3,15 +3,18 @@ import cors from 'cors'
 import 'dotenv/config'
 
 import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoutes.js';
  
 const PORT =process.env.PORT|| 4000;
 const app=express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
 await connectDB()
 
+app.use('/api/user',userRouter)
 app.get('/',(req,res)=>res.send("API Working fine"))
 
 app.listen(PORT,()=>console.log('Server running on port '+PORT))
