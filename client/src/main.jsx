f8939920d0497133121b540064fdbd0d43043b00
Ram Charan <ramcharanchary6300@gmail.com>
2025-04-28 12:28:56 +0530

    Fix bcrypt issue: clean install

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import AppContextProvider from "./context/AppContext.jsx";
 
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App.jsx";  
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
