530152cea7340ce3b4488c31c64847ac3fbc3b75
Ram Charan <ramcharanchary6300@gmail.com>
2025-04-28 10:36:46 +0530

    Refactors backend URL handling for environments

--- server/server.js
+++ server/server.js
@@ -5,8 +5,11 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import userRouter from './routes/userRoutes.js';
 import imageRouter from './routes/imageRoutes.js';
+
+import path from "path"
  
 const PORT =process.env.PORT|| 4000;
+const __dirname=path.resolve()
 const app=express()
 
 app.use(express.json())
@@ -18,5 +21,12 @@ await connectDB()
 app.use('/api/user',userRouter)
 app.use('/api/image',imageRouter)
 app.get('/',(req,res)=>res.send("API Working fine"))
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../client/dist")))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../client","dist","index.html"))
+    })
+}
 
 app.listen(PORT,()=>console.log('Server running on port '+PORT))
